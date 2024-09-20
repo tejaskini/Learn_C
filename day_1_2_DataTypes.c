265de3ab7eb4d089bae33f0e5354867c543fcccb
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-09-20 17:38:18 +0530

    Data Types and its format specifiers

---
+++ day_1_2_DataTypes.c
@@ -0,0 +1,29 @@
+// Data types in C
+
+#include <stdio.h>
+
+int main(){
+// int
+int myInt = 12;
+
+float myFloat = 12.22; //float
+
+char myChar = 'c'; // char 
+
+double myDouble = 56.4545454;
+
+//printing data types
+printf("%i \n", myInt);
+printf("%f \n", myFloat);
+printf("%c \n", myChar);
+printf("%lf \n", myDouble);
+
+
+// format specifiers ( in double quotes) 
+// %d or %i - integer
+// %f or %F - float
+// %c - char
+// %lf - double
+// %s - String
+
+}
